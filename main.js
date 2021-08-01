c7a90d3eae414c1fb2c684f5fa70a291ec507ce9
Jan Biernacik <jan.biernacik@e.email>
2021-08-01 20:52:31 +0200

    Verkackter responibility versuch
    nix funktioniert scheiß js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function mobile_resize() {
-/* Kommt noch */
+    if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i) 
+            || navigator.userAgent.match(/iPad/i) 
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)) {
+        var element = document.getElementById("mobile_navbar")
+        element.setAttribute("style", "display: inline;")
+    }
+    else {
+        var element = document.getElementById("desktop_navbar")
+        element.setAttribute("style", "display: inline;")
+    }
 }
 
 function small_window_size_resize() {
